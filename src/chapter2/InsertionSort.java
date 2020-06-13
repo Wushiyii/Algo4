56e2eaf8a418036e361b80d37b5c4630a91ffc7f
jessyy <wgq287676521@qq.com>
2020-06-13 22:27:08 +0800

    增加插入排序（O(N^2)）

--- src/chapter2/InsertionSort.java
+++ src/chapter2/InsertionSort.java
@@ -1,11 +1,27 @@
 package chapter2;
 
+/**
+ * 插入排序实现
+ * 平均复杂度（N^2/4） 最坏(N^2/2)  最好(N-1)
+ */
 public class InsertionSort extends AbstractSort {
 
 
     @Override
     public void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less(arr[j],arr[j-1])) {
+                    exchange(arr, j, j-1);
+                }
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] arr = new int[] {4,7,1,2,6,8,9,2,3};
+        AbstractSort compareSort = new InsertionSort();
+        compareSort.check(arr);
     }
 
 
