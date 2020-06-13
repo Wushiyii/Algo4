d58a249927e353bca5cffe8df3eef240a1165550
jessyy <wgq287676521@qq.com>
2020-06-13 23:48:14 +0800

    增加希尔排序（O(N^(2/3))）复杂度与h有关

---
+++ src/chapter2/ShellSort.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+public class ShellSort extends AbstractSort {
+
+    @Override
+    public void sort(int[] arr) {
+        int N = arr.length;
+        int h = 1;
+        while (h < N / 3) {
+            h = 3 * h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h ; j = j - h) {
+                    if (less(arr[j], arr[j - h])) {
+                        exchange(arr, j, j-h);
+                    }
+                }
+            }
+            h = h / 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {4,7,2,5,1,9,8,3,6,0};
+        AbstractSort compareSort = new ShellSort();
+        compareSort.check(arr);
+    }
+}
