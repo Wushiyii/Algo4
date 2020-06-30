4ce4845ee11e0449e3b9d2c520dca42b3cdd25ff
jessyy <wgq287676521@qq.com>
2020-06-30 22:11:51 +0800

    增加pathSearch接口定义

---
+++ src/chapter4/PathSearch.java
@@ -0,0 +1,24 @@
+package chapter4;
+
+/**
+ * @author wgq
+ * @date 2020/6/30 9:51 下午
+ */
+public interface PathSearch {
+
+    /**
+     * 是否存在从起点s到v的路径
+     * @param v v顶点
+     * @return 是否存在路径
+     */
+    boolean hasPathTo(int v);
+
+
+    /**
+     * 起点s到v的所有路径
+     * @param v 顶点v
+     * @return 所有路径，不存在则返回null
+     */
+    Iterable<Integer> pathTo(int v);
+
+}
